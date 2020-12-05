0cba34d481de3a7db1d46718977dbb3a61cbe1bb
Amaru <amarufd@gmail.com>
2020-12-05 09:48:31 -0300

    creando script extraccion yapo

---
+++ ExtraeYapo.R
@@ -0,0 +1,111 @@
+# configurando mi espacio de trabajo
+setwd("~/Documentos/UTEM/ICOS22020")
+
+# Borrando las variables de entorno
+rm(list = ls())
+
+# Importando las librerias
+library('rvest')
+library('gdata')
+
+# descargando la pagina yapo.cl
+yapo <- read_html('https://www.yapo.cl/region_metropolitana')
+
+# listado de productos
+listadoProductos <- html_nodes(yapo, css = "#hl")
+
+
+# titulos de los productos
+titulos <- html_nodes(listadoProductos,".title")
+textTitulos <- html_text(titulos)
+
+for (i in 1:length(textTitulos)) {
+  print(textTitulos[i])
+}
+
+# categoria
+categorias <- html_nodes(listadoProductos, css = ".category")
+textoCategorias <- html_text(categorias)
+
+for (i in 1:length(textoCategorias)) {
+  print(textoCategorias[i])
+}
+
+# micro estadistia
+table(textoCategorias)
+
+# comunas
+comunas <- html_nodes(listadoProductos, css = '.commune')
+textoComunas <- html_text(comunas)
+
+# otra forma de recorrer una variable con for
+for (textoComuna in textoComunas) {
+  print(textoComuna)
+}
+
+table(textoComunas)
+
+
+# precios
+precios <- html_nodes(listadoProductos,css = '.price')
+textoPrecios <- html_text(precios)
+
+
+for (i in 1:length(textoCategorias)) {
+  print("=========== item ===========")
+  print(textTitulos[i])
+  print(textoCategorias[i])
+  print(textoComunas[i])
+  print(textoPrecios[i])
+}
+
+
+# listado productos individuales
+listadoIndividual <- html_nodes(listadoProductos,css = '.ad')
+
+
+# [almacenando informacio 1] creacion del dataframe
+todaLaInformacion <- data.frame()
+
+for(producto in listadoIndividual){
+  print('================== ITEMS ==================')
+  
+  # titulo
+  titulo <- html_nodes(producto, css = '.title')
+  textoTitulo <- html_text(titulo)
+  print(textoTitulo)
+  
+  # Categoria
+  categoria <- html_nodes(producto, css = '.category')
+  textoCategoria <- html_text(categoria)
+  print(textoCategoria)
+  
+  # comuna
+  comuna <- html_nodes(producto, css = '.commune')
+  textoComuna <- html_text(comuna)
+  print(textoComuna)
+  
+  # Precio
+  precio <- html_nodes(producto, css = '.price')
+  textoPrecio <- html_text(precio)
+  if(length(textoPrecio) == 0){
+    textoPrecio <- NA
+  } else {
+    textoPrecio <- gsub("\n","",textoPrecio)
+    textoPrecio <- gsub("\t","",textoPrecio)
+    textoPrecio <- gsub("[$]","",textoPrecio)
+    textoPrecio <- gsub("[.]","",textoPrecio)
+    textoPrecio <- trim(textoPrecio)
+    textoPrecio <- as.numeric(textoPrecio)
+  }
+  print(textoPrecio)
+  
+  #[almacenando informacio 2] creando dataframe con los detalles
+  # de cada item
+  item <- data.frame(titulo = textoTitulo, categoria = textoCategoria, precio = textoPrecio, comuna = textoComuna)
+  
+  #[almacenando informacio 3] almacenando la informacion del producto
+  # con los datos totales
+  todaLaInformacion <- rbind(todaLaInformacion,item)
+  
+}
